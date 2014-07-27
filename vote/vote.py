b5c73ad94fd8e129ee62e9bea61c0658904b1b1d
hyxbiao <hyxbiao@gmail.com>
2014-07-27 15:06:43 +0800

    update

--- vote/vote.py
+++ vote/vote.py
@@ -91,12 +91,16 @@ def visitHome(cookie):
         "User-Agent": "Mozilla-Firefox5.0",
         "Host": "www.soompi.com",
     }
-    cjhandler=urllib2.HTTPCookieProcessor(cookie)
-    opener = urllib2.build_opener(cjhandler)
-    urllib2.install_opener(opener)
+    try:
+        cjhandler=urllib2.HTTPCookieProcessor(cookie)
+        opener = urllib2.build_opener(cjhandler)
+        urllib2.install_opener(opener)
 
-    req = urllib2.Request(url, None, header)
-    res = urllib2.urlopen(req)
+        req = urllib2.Request(url, None, header)
+        res = urllib2.urlopen(req)
+    except e:
+        print "[Error] %s" % str(e)
+        return False
     return res
 
 def checkIsVoted(cookie):
@@ -106,20 +110,24 @@ def checkIsVoted(cookie):
         "User-Agent": "Mozilla-Firefox5.0",
         "Host": "www.soompi.com",
     }
-    cjhandler=urllib2.HTTPCookieProcessor(cookie)
-    opener = urllib2.build_opener(cjhandler)
-    urllib2.install_opener(opener)
+    try:
+        cjhandler=urllib2.HTTPCookieProcessor(cookie)
+        opener = urllib2.build_opener(cjhandler)
+        urllib2.install_opener(opener)
 
-    req = urllib2.Request(url, None, header)
-    res = urllib2.urlopen(req)
-    if res.code != 200:
-        return False
-    content = res.read()
-    m = re.search('voted = (\d)', content)
-    if m:
-        #print "voted: %s" % m.group(1)
-        return int(m.group(1)) == 1
-    else:
+        req = urllib2.Request(url, None, header)
+        res = urllib2.urlopen(req)
+        if res.code != 200:
+            return False
+        content = res.read()
+        m = re.search('voted = (\d)', content)
+        if m:
+            #print "voted: %s" % m.group(1)
+            return int(m.group(1)) == 1
+        else:
+            return False
+    except e:
+        print "[Error] %s" % str(e)
         return False
     return False
 
@@ -175,19 +183,23 @@ def login(cookie, user, pwd):
         "Origin": "http://www.soompi.com",
         "Referer": "http://www.soompi.com/login/?redirect_to=%2Fseoul-international-drama-awards-2014-vote%2F%23voting"
     }
-    postdata = urllib.urlencode(data)
+    try:
+        postdata = urllib.urlencode(data)
 
-    #cj = cookielib.CookieJar()
-    cjhandler=urllib2.HTTPCookieProcessor(cookie)
-    opener = urllib2.build_opener(cjhandler)
-    urllib2.install_opener(opener)
+        #cj = cookielib.CookieJar()
+        cjhandler=urllib2.HTTPCookieProcessor(cookie)
+        opener = urllib2.build_opener(cjhandler)
+        urllib2.install_opener(opener)
 
-    req = urllib2.Request(url, postdata, header)
-    res = urllib2.urlopen(req)
-    if res.code != 200:
-        return False
-    content = res.read(1024)
-    if re.search('<title>Login', content):
+        req = urllib2.Request(url, postdata, header)
+        res = urllib2.urlopen(req)
+        if res.code != 200:
+            return False
+        content = res.read(1024)
+        if re.search('<title>Login', content):
+            return False
+    except e:
+        print "[ERROR] %s" % str(e)
         return False
     return True
 
@@ -198,14 +210,34 @@ def vote(cookie, url, data):
         "Host": "www.soompi.com",
         "Origin": "http://www.soompi.com",
     }
-    postdata = urllib.urlencode(data)
+    try:
+        postdata = urllib.urlencode(data)
 
+        #cj = cookielib.CookieJar()
+        cjhandler=urllib2.HTTPCookieProcessor(cookie)
+        opener = urllib2.build_opener(cjhandler)
+        urllib2.install_opener(opener)
+
+        req = urllib2.Request(url, postdata, header)
+        res = urllib2.urlopen(req)
+    except e:
+        print "[ERROR] %s" % str(e)
+        return False
+    return res
+
+def checkVoteDone(cookie, url):
+    header={
+        "User-Agent": "Mozilla-Firefox5.0",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Host": "www.soompi.com",
+        "Origin": "http://www.soompi.com",
+    }
     #cj = cookielib.CookieJar()
     cjhandler=urllib2.HTTPCookieProcessor(cookie)
     opener = urllib2.build_opener(cjhandler)
     urllib2.install_opener(opener)
 
-    req = urllib2.Request(url, postdata, header)
+    req = urllib2.Request(url, None, header)
     res = urllib2.urlopen(req)
     return res
 
@@ -240,7 +272,7 @@ def run(user, pwd):
     cookie = cookielib.CookieJar()
 
     res = visitHome(cookie)
-    if res.code != 200:
+    if not res or res.code != 200:
         print "Access home fail"
         return False, 2
 
@@ -264,10 +296,11 @@ def run(user, pwd):
         "myVotes": "213"
     }
     res = vote(cookie, url, data)
-    if res.code == 200:
-        print "Vote drama success"
+    if not res or res.code == 200:
+        print "Try to vote drama"
     else:
         print "Vote drama fail"
+        return False, 5
 
     url = "http://www.soompi.com/seoul-international-drama-awards-2014-vote/?category=actor&last=drama"
     data={
@@ -275,10 +308,11 @@ def run(user, pwd):
         "myVotes": "222"
     }
     res = vote(cookie, url, data)
-    if res.code == 200:
-        print "Vote actor success"
+    if not res or res.code == 200:
+        print "Try to vote actor"
     else:
         print "Vote actor fail"
+        return False, 6
 
     url = "http://www.soompi.com/seoul-international-drama-awards-2014-vote/?category=actress&last=actor"
     data={
@@ -286,13 +320,51 @@ def run(user, pwd):
         "myVotes": "225"
     }
     res = vote(cookie, url, data)
-    if res.code == 200:
-        print "Vote actress success"
+    if not res or res.code == 200:
+        print "Try to vote actress"
     else:
         print "Vote actress fail"
+        return False, 7
 
-    hadVoted(user)
-    return True, 0
+    url = "http://www.soompi.com/seoul-international-drama-awards-2014-vote/?category=drama&last=actress"
+    res = checkVoteDone(cookie, url)
+    data = res.read()
+    with open("debug_%s" % user, "w") as f:
+        f.write(data)
+        f.close()
+    if re.search("You're all done voting", data):
+        hadVoted(user)
+        return True, 0
+
+    return False, 10
+
+def runBatch(accounts, sleep_time, voted_accounts):
+    success = 0
+    fail_accounts = []
+
+    for item in accounts:
+        user = item[0]
+        pwd = item[1]
+        retry = 3
+        ret, status = False, 0
+        while retry > 0:
+            ret, status = run(user, pwd)
+            if ret or status == 4:
+                break
+            print "User: %s Vote fail, retry!" % user
+            retry -= 1
+        if ret:
+            success += 1
+            print "User: %s vote success!" % user
+        elif status == 4:
+            voted_accounts.append([user, pwd])
+        else:
+            fail_accounts.append([user, pwd])
+
+        if sleep_time != 0:
+            time.sleep(sleep_time)
+
+    return success, fail_accounts
 
 def main():
     socket.setdefaulttimeout(30)
@@ -310,43 +382,32 @@ def main():
 
     total = len(accounts)
     success = 0
-    has_voted = 0
-    fail_accounts = []
-
-    for item in accounts:
-        user = item[0]
-        pwd = item[1]
-        retry = 3
-        ret, status = False, 0
-        while retry > 0:
-            ret, status = run(user, pwd)
-            if ret or status == 4:
-                break
-            print "User: %s Vote fail, retry!" % user
-            retry -= 1
-        if ret:
-            success += 1
-        else:
-            fail_accounts.append([user, pwd, status])
-
-        if sleep_time != 0:
-            time.sleep(sleep_time)
+    voted_accounts = []
+    retry = 3
+    while retry > 0:
+        nsuccess, fail_accounts = runBatch(accounts, sleep_time, voted_accounts)
+        success += nsuccess
+        if len(fail_accounts) == 0:
+            break
+        accounts = fail_accounts
+        retry -= 1
 
     rf = open("finish.txt", "w")
-    print "\n\n------------------- FINISH -------------------"
+    print "\n\n------------------- Had Voted -------------------"
+    for item in voted_accounts:
+        user = item[0]
+        pwd = item[1]
+        txt = "[Voted] %s" % user
+        print txt
+        rf.write(txt + "\n")
+    print "\n------------------- Fail -------------------"
     for item in fail_accounts:
         user = item[0]
         pwd = item[1]
-        status = item[2]
-        if status == 4:
-            has_voted += 1
-            txt = "[Voted] %s" % user
-        else:
-            txt = "[Fail ] %s" % user
+        txt = "[Fail ] %s" % user
         print txt
         rf.write(txt + "\n")
-    fail = total - success - has_voted
-    txt = "\nAccounts total: %d, success: %d, fail: %d, had voted: %d" % (total, success, fail, has_voted)
+    txt = "\nAccounts total: %d, success: %d, fail: %d, had voted: %d" % (total, success, len(fail_accounts), len(voted_accounts))
     print txt
     rf.write(txt + "\n")
     rf.close()
