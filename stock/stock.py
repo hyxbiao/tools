aa166dd2eaaf740d4e987b7605d8cb86da2cb484
unknown <biao@biao-PC.(none)>
2015-05-28 00:27:58 +0800

    update

--- stock/stock.py
+++ stock/stock.py
@@ -4,7 +4,40 @@ import struct
 
 def debug(data):
     print ":".join(c.encode('hex') for c in data)
-    
+
+def macd(EMA_S, EMA_L, DEA, closeq, S=12, L=26, M=9):
+    rS = S + 1
+    rL = L + 1
+    rM = M + 1
+    rEMA_S = EMA_S + (closeq - EMA_S) * 2 / rS
+    rEMA_L = EMA_L + (closeq - EMA_L) * 2 / rL
+    rDIFF = rEMA_S - rEMA_L
+    rDEA = DEA * 0.8 + rDIFF * 0.2
+    rBAR = 2 * (rDIFF - rDEA)
+
+    return rEMA_S, rEMA_L, rDIFF, rDEA, rBAR
+
+def kdj(data, K, D, N=9, M1=3, M2=3):
+    datalen = len(data)
+
+    mins = 100000.0
+    maxs = 0.0
+    for item in data[-N:]:
+        mins = min(mins, item[3])
+        maxs = max(maxs, item[2])
+
+    rRSV = 100 * (data[-1][4] - mins) / (maxs - mins)
+    if datalen == 1:
+        rK = rRSV
+        rD = rRSV
+    else:
+        rK = float(M1-1)/M1 * K + 1.0/M1 * rRSV
+        rD = float(M2-1)/M2 * D + 1.0/M2 * rK
+    rJ = 3 * K - 2 * D
+
+    print rK, rD, rJ
+    return rK, rD, rJ
+
 def main():
     filename = '600030.day'
     #filename = unicode(filename , "utf-8")
@@ -20,33 +53,37 @@ def main():
         cols_f = struct.unpack('%dI' % col_count, f.read(col_length))
         f.seek(start)
 
-        DIFF = 0.0
-        DEA = 0.0
-        MACD = 0.0
-        for i in xrange(count):
-            cols = struct.unpack('%dI' % col_count, f.read(clen))
-            date = cols[0]
-            openq = float(cols[1] & 0xFFFFFF) / 1000
-            high = float(cols[2] & 0xFFFFFF) / 1000
-            low = float(cols[3] & 0xFFFFFF) / 1000
-            closeq = float(cols[4] & 0xFFFFFF) / 1000
-            money = float(cols[5] & 0xFFFFFF) / 1000
-            volume = float(cols[6] & 0xFFFFFF) / 1000
-            
-            print date, openq, high, low, closeq, money, volume
-            if i == 0:
-                EMA12 = closeq
-                EMA26 = closeq
-            else:
-                EMA12 = EMA12 + (closeq - EMA12) * 2 / 13
-                EMA26 = EMA26 + (closeq - EMA26) * 2 / 27
-                DIFF = EMA12 - EMA26
-                DEA = DEA * 0.8 + DIFF * 0.2
-                BAR = 2 * (DIFF - DEA)
-                
-                print EMA12, EMA26, DIFF, DEA, BAR
+        rawdata = f.read(count * clen)
+        f.close()
 
-            print '-----------------------------'
+    data = []
+    for i in xrange(count):
+        pos = i * clen
+        cols = struct.unpack('7I', rawdata[pos: pos+28])
+        date = cols[0]
+        openq = float(cols[1] & 0xFFFFFF) / 1000
+        high = float(cols[2] & 0xFFFFFF) / 1000
+        low = float(cols[3] & 0xFFFFFF) / 1000
+        closeq = float(cols[4] & 0xFFFFFF) / 1000
+        money = float(cols[5] & 0xFFFFFF) / 1000
+        volume = float(cols[6] & 0xFFFFFF) / 1000
+
+        data.append([date, openq, high, low, closeq, money, volume])
+        print date, openq, high, low, closeq, money, volume
+        if i == 0:
+            EMA12 = closeq
+            EMA26 = closeq
+            DEA = 0.0
+
+            K = 50
+            D = 50
+        else:
+            EMA12, EMA26, DIFF, DEA, BAR = macd(EMA12, EMA26, DEA, closeq)
+            
+            print EMA12, EMA26, DIFF, DEA, BAR
+
+        K, D, J = kdj(data, K, D)
+        print '-----------------------------'
 
         f.close()
     print 'test'
